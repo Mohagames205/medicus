045781625efab1bceea42b091dfa61ebbd3d2c50
Mohamed <mohamedelyousfi87@gmail.com>
2023-10-07 22:38:34 +0200

    Load ICS from link and idk some other stuff too

--- bot.py
+++ bot.py
@@ -14,6 +14,17 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+
+class CourseEvent:
+    NO_EVENT = -1
+    UPCOMING = 0
+    CURRENT = 1
+
+    def __init__(self, event: Event, status: int):
+        self.event = event
+        self.status = status
+
+
 intents = discord.Intents.all()
 client = commands.Bot(command_prefix='$', intents=intents)
 tree = client.tree
@@ -24,8 +35,10 @@ con = sqlite3.connect("bot.db")
 con.row_factory = sqlite3.Row  # https://stackoverflow.com/questions/3300464/how-can-i-get-dict-from-sqlite-query
 cur = con.cursor()
 cur.execute(
-    'CREATE TABLE IF NOT EXISTS subscribed_messages (id INTEGER PRIMARY KEY, channel_id INTEGER, message_id INTEGER, guild_id INTEGER, phase INTEGER)')
+    'CREATE TABLE IF NOT EXISTS subscribed_messages (id INTEGER PRIMARY KEY, channel_id INTEGER, message_id INTEGER, guild_id INTEGER, phase INTEGER);')
 
+cur.execute(
+    'CREATE TABLE IF NOT EXISTS calendars (id INTEGER PRIMARY KEY, link TEXT, phase INTEGER UNIQUE);')
 
 # tijdelijke hack
 
@@ -34,8 +47,10 @@ tijd = {
     "maand": 10,
     "dag": 10,
     "uur": 8,
-    "minuut": 30
+    "minuut": 30,
+    "override": False
 }
+
 
 # sync the slash command to your server
 @client.event
@@ -48,59 +63,93 @@ async def on_ready():
 
 
 async def get_file_content(url):
-    response = await aiohttp.ClientSession().get(url)
-    return discord.File(fp=io.BytesIO(await response.read()), filename="motivatie.mp3")
+    # thx https://github.com/Azure/azure-sdk-for-python/issues/13242#issuecomment-1239061801
+    session = aiohttp.ClientSession()
+    response = await session.get(url)
+
+    content = await response.read()
+
+    await session.close()
+    return content.decode('utf-8')
 
 
-@client.hybrid_command(name="motivatie", description="Krijg motivatie van de enige echte motivator")
-async def motivatie(ctx):
-    file_content = await get_file_content(
-        "https://cdn.discordapp.com/attachments/1122300299604918354/1123175156047695912/Messenger_Facebook.mp3?ex=65193460&is=6517e2e0&hm=7753692951a5f79a0a7e1419082bc1a60c8bb52a3edba854ef1d00b7a8c648f3&")
-    await ctx.send("Als u een vraag niet weet, niet panikeren, maar altijd door doen, nooit opgeven")
-    await ctx.channel.send(file=file_content)
+@tree.command(name="providecal")
+@commands.has_permissions(kick_members=True)
+async def provice_ics(int: discord.Interaction, link: str, phase: int):
+    await register_calendar(link, phase)
+    await int.response.send_message("ICS has been registered succesfully", ephemeral=True)
 
 
 @tree.command(name="setschedulechannel")
-async def set_schedule_channel(int: discord.Interaction, fase: int):
+async def set_schedule_channel(int: discord.Interaction, phase: int):
     embed = discord.Embed(
         title="Huidig hoorcollege",
-        description="Hier zie je informatie over het huidig hoorcollege.",
-        color=discord.Color.blue()
+        color=discord.Color.pink()
     )
 
     global_embed_msg = await int.channel.send(embed=embed)
-    await register_message(fase, global_embed_msg)
+    await register_message(phase, global_embed_msg)
 
     await int.response.send_message("test")
 
 
-async def get_event_at(time: Arrow):
-    with open('cal.ics', 'r') as file:
-        ics_text = file.read()
+async def get_event_at(time: Arrow, phase: int):
+    file = await get_file_content(await fetch_calendar(phase))
+    cal = Calendar(file)
 
-    cal = Calendar(ics_text)
+    events = sorted(cal.events, key=lambda ev: ev.begin)
 
-    for event in cal.events:
+    ongoing_event = None
+    next_event = None
+
+    for event in events:
         if event.begin <= time <= event.end:
-            return event
+            ongoing_event = event
 
-    return Event("Geen hoorcollege")
+        elif event.begin > time:
+            next_event = event
+            break
+
+    if ongoing_event:
+        return CourseEvent(ongoing_event, CourseEvent.CURRENT)
+    elif next_event:
+        return CourseEvent(next_event, CourseEvent.UPCOMING)
+    else:
+        return CourseEvent(Event("Geen hoorcollege"), CourseEvent.NO_EVENT)
 
 
-async def update_embed(embed_message, ongoing_event: Event):
+async def update_embed(embed_message, course_event: CourseEvent):
+    if course_event.status == CourseEvent.CURRENT:
+        title = "🎯  |  Huidig hoorcollege"
+        color = discord.Color.purple()
+    elif course_event.status == CourseEvent.UPCOMING:
+        title = "➡️  |  Toekomstig hoorcollege"
+        color = discord.Color.green()
+    else:
+        title = "Geen hoorcollege ❌"
+        color = discord.Color.red()
+
+    ongoing_event = course_event.event
+
     embed = discord.Embed(
-        title="Huidig hoorcollege",
-        color=discord.Color.red()  # You can customize the color
+        title=title,
+        color=color
     )
+
+    duration_str = str(ongoing_event.duration)
+    hours, minutes, _ = duration_str.split(":")  # Splitting hours, minutes, and seconds
+
+    # Formatting the duration
+    formatted_duration = f"{hours}u{minutes}m"
 
     embed.add_field(name="Hoorcollege", value=f"{ongoing_event.name}")
 
     if ongoing_event.name != 'Geen hoorcollege':
-        embed.color = discord.Color.blue()
         embed.add_field(name="Locatie", value=f"{ongoing_event.location}", inline=False)
-        embed.add_field(name="Start", value=f"{ongoing_event.begin}", inline=False)
-        embed.add_field(name="Einde", value=f"{ongoing_event.end}", inline=False)
-        embed.add_field(name="Duur", value=f"{ongoing_event.duration}", inline=False)
+        embed.add_field(name="Tijd",
+                        value=f"{ongoing_event.begin.format('HH:mm', 'nl')} - {ongoing_event.end.format('HH:mm', 'nl')}  |  {ongoing_event.end.format('D MMMM YYYY', 'nl')}",
+                        inline=False)
+        embed.add_field(name="Duur", value=f"{formatted_duration}", inline=False)
         embed.add_field(name="Beschrijving", value=f"{ongoing_event.description}", inline=False)
 
     await embed_message.edit(embed=embed)
@@ -113,19 +162,46 @@ async def override_time(interaction: discord.Interaction, jaar: int, maand: int,
     tijd["dag"] = dag
     tijd["uur"] = uur
     tijd["minuut"] = minuut
+    tijd["override"] = True
 
     await interaction.response.send_message("Time has been succesfully overriden")
 
-@tasks.loop(seconds=10)
+
+@tree.command(name="clearoverride")
+async def clear_override(interaction: discord.Interaction):
+    tijd["override"] = False
+    await interaction.response.send_message("Time has been succesfully reset")
+
+
+@tasks.loop(seconds=30)
 async def check_ical():
+    print("fetching updates...")
     for guild in client.guilds:
 
         for embed_message in await fetch_messages(guild):
+            message = embed_message["message"]
+            phase = embed_message["phase"]
+
             brussels_timezone = pytz.timezone('Europe/Brussels')
 
-            print("going to update...")
-            now = Arrow.fromdatetime(datetime(tijd["jaar"], tijd["maand"], tijd["dag"], tijd["uur"], tijd["minuut"]), tzinfo=brussels_timezone)
-            await update_embed(embed_message, await get_event_at(now))
+            now = Arrow.now()
+
+            if tijd["override"]:
+                now = Arrow.fromdatetime(
+                    datetime(tijd["jaar"], tijd["maand"], tijd["dag"], tijd["uur"], tijd["minuut"]),
+                    tzinfo=brussels_timezone)
+            await update_embed(message, await get_event_at(now, phase))
+
+
+async def register_calendar(link: str, phase: int):
+    cur.execute('INSERT INTO calendars (`link`, `phase`) values (?, ?)',
+                (link, phase))
+    con.commit()
+
+
+async def fetch_calendar(phase: int):
+    cur.execute('SELECT * FROM calendars WHERE `phase` = ?', (phase,))
+    return cur.fetchone()["link"]
 
 
 async def fetch_messages(guild: discord.Guild):
@@ -138,7 +214,8 @@ async def fetch_messages(guild: discord.Guild):
         guild = client.get_guild(result["guild_id"])
         channel = guild.get_channel(result["channel_id"])
         message = await channel.get_partial_message(result["message_id"]).fetch()
-        messages.append(message)
+
+        messages.append({"message": message, "phase": result["phase"]})
 
     return messages
 
