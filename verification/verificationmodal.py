0f2e0791e6d3daa929442d086004a8871472bd7a
Mohamed El Yousfi <mohamedelyousfi87@gmail.com>
2023-10-27 16:45:01 +0200

    VerificationButton should be it's own class, to make code less complex

--- verification/verificationmodal.py
+++ verification/verificationmodal.py
@@ -5,11 +5,23 @@ from verification import verification
 
 
 class VerificationButton(ui.Button):
-    def __init__(self, label, modal: ui.Modal):
-        super().__init__(label=label)
+    def __init__(self, label, modal, verification_module):
+        super().__init__(label=label, emoji="📥")
         self.modal = modal
+        self.verification_module = verification_module
+
     async def callback(self, interaction: discord.Interaction):
-        await interaction.response.send_modal(self.modal)
+        await interaction.response.send_modal(self.modal(self.verification_module))
+
+
+class InputCodeButton(ui.Button):
+    def __init__(self, student, verification_module):
+        super().__init__(label="Code invullen", emoji="🔐")
+        self.student = student
+        self.verification_module = verification_module
+
+    async def callback(self, interaction: discord.Interaction):
+        await interaction.response.send_modal(VerificationModal(self.student, self.verification_module))
 
 
 class CollectNameModal(ui.Modal, title="Geef je voor- en achternaam"):
@@ -30,10 +42,19 @@ class CollectNameModal(ui.Modal, title="Geef je voor- en achternaam"):
             self.verification_module.send_mail(student.email, code)
 
             view = ui.View()
-            view.add_item(VerificationButton("Code invullen", VerificationModal(student, self.verification_module)))
-            await interaction.channel.send_message("Gelieve je mailbox te checken voor een verificatiecode. Druk vervolgens op de onderstaande knop.", view=view)
+            view.add_item(InputCodeButton(student, self.verification_module))
+            await interaction.response.send_message(
+                "Gelieve je studentenmail te checken voor een verificatiecode. Druk vervolgens op de onderstaande knop.",
+                view=view, ephemeral=True)
         else:
-            await interaction.response.send_message("Volgens onze gegevens ben je geen student geneeskunde. Indien dit een vergissing is, contacteer dan zeker de beheerders of moderators. Zij helpen je graag verder op weg.")
+            embed = discord.Embed(
+                title="Geen student bachelor geneeskunde",
+                description="Je bent geen (bachelor) geneeskunde student aan de KU Leuven. Indien je denkt dat dit een vergissing is, neem dan contact op met een van de beheerders of moderators.",
+                color=discord.Color.red()
+            )
+
+            await interaction.response.send_message(embed=embed, ephemeral=True)
+
 
 class VerificationModal(ui.Modal, title='Verificatiecode studentenmail'):
     code = ui.TextInput(label='Verificatiecode')
@@ -44,9 +65,8 @@ class VerificationModal(ui.Modal, title='Verificatiecode studentenmail'):
         self.student = student
 
     async def on_submit(self, interaction: discord.Interaction):
-        # check of het juist is
-
-        self.verification_module.cur.execute("SELECT code FROM verification_codes WHERE email = ?", (self.student.email,))
+        self.verification_module.cur.execute("SELECT code FROM verification_codes WHERE email = ?",
+                                             (self.student.email,))
         result = self.verification_module.cur.fetchone()
 
         inputted_code = int(self.code.value)
@@ -66,4 +86,7 @@ class VerificationModal(ui.Modal, title='Verificatiecode studentenmail'):
             await interaction.user.send(embed=embed)
             await interaction.followup.send(f"Dank je wel! Je bent succesvol geverifieerd :)", ephemeral=True)
         else:
-            await interaction.followup.send(f"De code die je hebt opgegeven is incorrect.", ephemeral=True)
+            view = ui.View()
+            view.add_item(InputCodeButton(self.student, self.verification_module))
+
+            await interaction.followup.send(f"De code die je hebt opgegeven is incorrect. Probeer het opnieuw:", ephemeral=True, view=view)
